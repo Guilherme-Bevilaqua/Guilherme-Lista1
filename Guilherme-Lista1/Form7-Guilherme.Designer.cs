5d3a034a12112cda63bbe1a997ac87a974082946
Aluno <Aluno@LAB02-NOTE-06>
2025-04-16 14:33:55 -0300

    alteração no 7

--- Guilherme-Lista1/Form7-Guilherme.Designer.cs
+++ Guilherme-Lista1/Form7-Guilherme.Designer.cs
@@ -52,9 +52,10 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Location = new System.Drawing.Point(16, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(683, 35);
+            this.label1.Size = new System.Drawing.Size(911, 43);
             this.label1.TabIndex = 4;
             this.label1.Text = "O programa é responsavel por Calcular os litros gastos na viagem";
             // 
@@ -63,35 +64,39 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(11, 79);
+            this.label2.Location = new System.Drawing.Point(15, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(433, 31);
+            this.label2.Size = new System.Drawing.Size(700, 38);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Digite o Tempo gasto na viagem:";
+            this.label2.Text = "Digite o Tempo em Horas gastas na viagem:";
             // 
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(11, 125);
+            this.label3.Location = new System.Drawing.Point(15, 154);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(351, 31);
+            this.label3.Size = new System.Drawing.Size(573, 38);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Digite a velocidade média: ";
+            this.label3.Text = "Digite a velocidade média em Km/h: ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(437, 90);
+            this.textBox1.Location = new System.Drawing.Point(723, 113);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(354, 136);
+            this.textBox2.Location = new System.Drawing.Point(596, 170);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 2;
             // 
             // button1
@@ -99,9 +104,10 @@
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(191, 193);
+            this.button1.Location = new System.Drawing.Point(255, 238);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 28);
+            this.button1.Size = new System.Drawing.Size(124, 34);
             this.button1.TabIndex = 3;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = false;
@@ -112,9 +118,10 @@
             this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(337, 190);
+            this.button2.Location = new System.Drawing.Point(449, 234);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(93, 31);
+            this.button2.Size = new System.Drawing.Size(124, 38);
             this.button2.TabIndex = 4;
             this.button2.Text = "Limpar";
             this.button2.UseVisualStyleBackColor = false;
@@ -125,9 +132,10 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(11, 297);
+            this.label4.Location = new System.Drawing.Point(15, 366);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(466, 31);
+            this.label4.Size = new System.Drawing.Size(621, 38);
             this.label4.TabIndex = 11;
             this.label4.Text = "a quantidade de litros gastos será:";
             // 
@@ -135,9 +143,10 @@
             // 
             this.label5.BackColor = System.Drawing.SystemColors.Window;
             this.label5.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label5.Location = new System.Drawing.Point(460, 305);
+            this.label5.Location = new System.Drawing.Point(613, 375);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.Size = new System.Drawing.Size(133, 28);
             this.label5.TabIndex = 12;
             // 
             // label6
@@ -145,9 +154,10 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(11, 340);
+            this.label6.Location = new System.Drawing.Point(15, 418);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(195, 31);
+            this.label6.Size = new System.Drawing.Size(260, 38);
             this.label6.TabIndex = 13;
             this.label6.Text = "tempo gasto:";
             // 
@@ -155,9 +165,10 @@
             // 
             this.label7.BackColor = System.Drawing.SystemColors.Window;
             this.label7.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label7.Location = new System.Drawing.Point(188, 348);
+            this.label7.Location = new System.Drawing.Point(251, 428);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.Size = new System.Drawing.Size(133, 28);
             this.label7.TabIndex = 14;
             // 
             // label8
@@ -165,9 +176,10 @@
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.Black;
-            this.label8.Location = new System.Drawing.Point(11, 380);
+            this.label8.Location = new System.Drawing.Point(15, 468);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(236, 31);
+            this.label8.Size = new System.Drawing.Size(315, 38);
             this.label8.TabIndex = 15;
             this.label8.Text = "Velocidade Média:";
             // 
@@ -175,9 +187,10 @@
             // 
             this.label9.BackColor = System.Drawing.SystemColors.Window;
             this.label9.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label9.Location = new System.Drawing.Point(240, 388);
+            this.label9.Location = new System.Drawing.Point(320, 478);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(100, 23);
+            this.label9.Size = new System.Drawing.Size(133, 28);
             this.label9.TabIndex = 16;
             // 
             // label10
@@ -185,9 +198,10 @@
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.Black;
-            this.label10.Location = new System.Drawing.Point(376, 340);
+            this.label10.Location = new System.Drawing.Point(501, 418);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(269, 31);
+            this.label10.Size = new System.Drawing.Size(359, 38);
             this.label10.TabIndex = 17;
             this.label10.Text = "Distancia Percorrida:";
             // 
@@ -195,9 +209,10 @@
             // 
             this.label11.BackColor = System.Drawing.SystemColors.Window;
             this.label11.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label11.Location = new System.Drawing.Point(640, 348);
+            this.label11.Location = new System.Drawing.Point(853, 428);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(100, 23);
+            this.label11.Size = new System.Drawing.Size(133, 28);
             this.label11.TabIndex = 18;
             // 
             // button3
@@ -206,9 +221,10 @@
             this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(499, 190);
+            this.button3.Location = new System.Drawing.Point(665, 234);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(93, 31);
+            this.button3.Size = new System.Drawing.Size(124, 38);
             this.button3.TabIndex = 5;
             this.button3.Text = "Sair";
             this.button3.UseVisualStyleBackColor = false;
@@ -217,12 +233,12 @@
             // Form7_Guilherme
             // 
             this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Guilherme_Lista1.Properties.Resources.fundoc_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.CancelButton = this.button3;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
@@ -241,11 +257,13 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form7_Guilherme";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form7_Guilherme";
+            this.Load += new System.EventHandler(this.Form7_Guilherme_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Guilherme-Lista1/Form7-Guilherme.cs
+++ Guilherme-Lista1/Form7-Guilherme.cs
@@ -30,9 +30,9 @@ namespace Guilherme_Lista1
             nLitrosGasto = nDistancia / 12;
             label5.Text = nLitrosGasto.ToString();
 
-            label7.Text = nTempo.ToString();
-            label9.Text = nVelocidadeMedia.ToString();
-            label11.Text = nDistancia.ToString();
+            label7.Text = nTempo.ToString() + nTempo.ToString(" Horas ");
+            label9.Text = nVelocidadeMedia.ToString() + nVelocidadeMedia.ToString(" Km/h ");
+            label11.Text = nDistancia.ToString() + nDistancia.ToString("Km");
 
         }
 
@@ -53,6 +53,11 @@ namespace Guilherme_Lista1
         {
 
             Application.Exit();
+
+        }
+
+        private void Form7_Guilherme_Load(object sender, EventArgs e)
+        {
 
         }
     }
